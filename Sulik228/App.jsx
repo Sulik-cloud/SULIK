a51a793bb4ef94a32caaeca439c62bc11576b6a6
Sulik-cloud <cekbrfwfe@gmail.com>
2026-03-04 22:03:39 +0200

    Create App.jsx file that imports and renders UmbraedonusModel component.

---
+++ Sulik228/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UmbraedonusModel from './UmbraedonusModel';
+
+const App = () => {
+    return (
+        <div>
+            <UmbraedonusModel />
+        </div>
+    );
+};
+
+export default App;
